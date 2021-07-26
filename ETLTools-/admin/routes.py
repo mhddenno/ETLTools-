1eab632af5888c04c2103b9c88d2660ab11365bd
Mohamad Denno <dennokhmhd@gmail.com>
2021-07-26 11:01:48 +0200

    Stable version]

--- ETLTools-/admin/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-admin = Blueprint('admin', __name__)
-
-@admin.route('/')
-def index():
-	return "Hello, World! This is the admin page."
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from project import app
+from ETLTools import app
 
 if __name__=='__main__':
 	app.run()
